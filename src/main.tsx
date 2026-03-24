383ce8f8d515321cd63c1e5de7b35e8dde5b5439
Wilson Li <wyli@uwaterloo.ca>
2026-03-24 15:52:42 -0400

    cicd and vercel setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
@@ -12,5 +13,6 @@ createRoot(document.getElementById('root')!).render(
         <App />
       </BrowserRouter>
     </ErrorBoundary>
+    <Analytics />
   </StrictMode>,
 )
